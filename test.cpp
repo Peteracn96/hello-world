37d08615a916f8553126ed4602e91ac82bff65e6
Peteracn96 <pacn96@gmail.com>
2020-11-12 17:36:34 +0000

    mandar test.cpp para o github

---
+++ test.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+class Rectangle {
+
+private:
+	size_t height;
+	size_t width;
+
+public: 
+	Rectangle() : height(2), width(3) { height = 4; width = 5;}
+	Rectangle(size_t a, size_t b){ height = a; width = b;}
+	size_t Area() {return height*width;}
+};
+
+int main(int argc, char const *argv[])
+{
+	Rectangle rect1;
+	Rectangle rect2 {1,7};
+
+	cout << "Area of rect1 = " << rect1.Area() << endl;
+	cout << "Area of rect2 = " << rect2.Area() << endl;
+	return 0;
+}
